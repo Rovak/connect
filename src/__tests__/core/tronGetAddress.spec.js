1638cbc2ce5abceab28d75eaeb2fa8cdd3c0cb8d
Roy van Kaathoven <ik@royvankaathoven.nl>
2020-01-23 13:32:47 +0800

    fix get address test

--- src/__tests__/core/tronGetAddress.spec.js
+++ src/__tests__/core/tronGetAddress.spec.js
@@ -12,15 +12,7 @@ export const tronGetAddress = (): TestFunction => {
     const testPayloads: Array<TestTronGetAddressPayload> = [
         {
             method: 'tronGetAddress',
-            path: "m/44'/60'/1",
-        },
-        {
-            method: 'tronGetAddress',
-            path: "m/44'/60'/0/1'",
-        },
-        {
-            method: 'tronGetAddress',
-            path: "m/44'/60'/0'/9'/0",
+            path: "m/44'/195'/0'/0/0",
         },
         {
             method: 'tronGetAddress',
@@ -34,17 +26,7 @@ export const tronGetAddress = (): TestFunction => {
     const expectedResponses: Array<ExpectedTronGetAddressResponse> = [
         {
             payload: {
-                address: '0xeD46C856D0c79661cF7d40FFE0C0C5077c00E898',
-            },
-        },
-        {
-            payload: {
-                address: '0x6682Fa7F3eC58581b1e576268b5463B4b5c93839',
-            },
-        },
-        {
-            payload: {
-                address: '0x2cffCE5B7DA9584caD519EFc4715425b630CEF3a',
+                address: 'TTmw3iwd5MXVfHxrM1ZDZ2sYddS2xZr4bk',
             },
         },
         { success: false },
